04268111fbb39e6b59d0cf2aa25cef1f97d1b90c
Danniel Christensen <dannielchristensen1@gmail.com>
2021-04-05 16:07:46 -0500

    proj2solution == update

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -2,14 +2,6 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
-
-/* --------- Karijanna's code starts here ---------- */
-typedef int pid_t;
-void halt(void);
-void exit (int status);
-pid_t exec (const char *cmd_line);
-int wait (pid_t pid);
-bool create (const char *file, unsigned initial_size);
-bool remove (const char *file);
+void syscall_exit (void);
 
 #endif /* userprog/syscall.h */
